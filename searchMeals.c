1bd952a24c60ee83ca1843d242e9ac6704a5b5cc
Andersraae <andersraae01@gmail.com>
2022-11-17 11:33:56 +0100

    searchMeals er lavet, men med minimal kode

---
+++ searchMeals.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "data.c"
+
+// int size = sizeof(ingredients)/sizeof(ingredients[0]);
+//     int ingredientsid[size];
+//     for (int i = 0; i < size ; i++)
+//     {
+        
+//     }
