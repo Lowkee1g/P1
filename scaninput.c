9725616a0d69c308ccab5cb242f4ff4d64a1aa1a
Lowkee1g <59569937+Lowkee1g@users.noreply.github.com>
2022-11-18 12:27:40 +0100

    .

--- scaninput.c
+++ scaninput.c
@@ -2,16 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 
-char* inputString;
+char inputString[50];
+char *inputArray;
 
-typedef struct {
-    char name[30];
-    int numberOf;
-} InputIngredience;
+void BlueOuputColor() {
+    printf("\033[0;36m");
+}
+void ResetOutputColor() {
+    printf("\033[0m");
+}
+void Debug(char s[100]) {
+    printf("\033[0;35m");
+    printf("     %c <%s>",175,s);
+    ResetOutputColor();
+}
 
 int main(void)
 {
     scanData();
+
+    Debug("This is an Debug");
 
     
     return 0;
@@ -21,26 +31,17 @@ int main(void)
 
 
 void scanData() {
-    
-    InputIngredience *Input = malloc(sizeof(InputIngredience));
-    //Input *input = malloc(sizeof(Input));
+    //Peudokode
+    /*Scanne efter et input
+    Tager inputtet og ændre længende af arrayet ved at sætte det ind efter
+    Loop vi op til start */
 
-    //inputs = (int *) malloc(1);
-    //Input->name = "Hello";
-    //Input->numberOf = 2;
+    int **array;
 
-    //printf("%d",Input->numberOf);
-    gets(inputString);
-    do
-    {
-        
-        
-    } while (inputString != "end");
-    
-    
+    array = malloc(10 * sizeof *array);
 
-    
+    for (int i = 0; i < )
 
-    
-    free(Input);
+
+
 }
