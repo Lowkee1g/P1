5de6f5915ae8319ca5868b3724c407ca1bb36b5b
Andersraae <andersraae01@gmail.com>
2022-11-22 12:18:22 +0100

    searchMeals.h er lavet. Søgefunktion er lavet, men ikke testet

--- searchMeals.c
+++ searchMeals.c
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include "data.h"
 
+void searchMeals(char* ings);
+int contains(int a, int *list);
+
 void searchMeals(char* ings){
     int size = sizeof(ings)/sizeof(ings[0]);
     Meals **foundmeals = (Meals *) malloc(size * sizeof(Meals)); 
@@ -20,12 +23,35 @@ void searchMeals(char* ings){
         }
     }
 
+    int counter; 
     for (int i = 0; i < size ; i++){ // Itererer gennem listen af input ingredienser
+    counter = 0;
         for (int j = 0; j < mealssize; j++) { // Itererer gennem alle meals for at finde nogle med ingredienser, som matcher
-            // if (meals[j].ings.Contains(ings[i])){ //Der er noget der ikke helt fungerer her...
-                
-            // }
+            if (contains(ings[i], meals[j].ings)){ //Der er noget der ikke helt fungerer her...
+                foundmeals[i][counter] = meals[j];
+                counter++;
+            }
             
         }
     }
+
+
+    //  Hvis det er en mulighed at lave første liste i foundmeals til at være de retter, som har flest til fælles, så kan search og sort meals
+    // være den samme funktion. https://stackoverflow.com/questions/58306236/can-i-use-contains-for-integer-list .... Det kan man sikkert ikke,
+    // men nu er linket her
+
+    // This tho https://www.tutorialkart.com/c-programming/c-check-if-array-contains-specified-element/
+}
+
+int contains(int a, int *list){
+    int size = sizeof list / sizeof list[0];
+    int isElementPresent = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (list[i] == a) {
+            isElementPresent = 1;
+            return 1;
+        }
+    }
+    return 0;
 }
