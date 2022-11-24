43a26ec25d1063477405e76f4dc67a2e887a99e4
davidfraenkel <david.fraenkel@hotmail.com>
2022-11-24 11:39:38 +0100

    Help me with fscanf

--- data.c
+++ data.c
@@ -2,8 +2,8 @@
 #include "data.h"
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
-int mealssize = 7;
 int ingredientssize = 7;
 Meals *meals; 
 Ingredients *ingredients;
@@ -11,55 +11,85 @@ Ingredients *ingredients;
 void changeMeal(int id, char* name, double price, int*  ingredientids);
 void changeIngredient(int id, char* name, double price);
 void initializeStructs();
-
+void InitializeMeals(FILE *file);
 void initializeStructs(){
-    meals = (Meals *) malloc(mealssize * sizeof(Meals));
+    
+    // int *ingredientids = (int *) malloc(3 * sizeof(int));
+    // if (ingredientids == NULL){
+    //     exit(EXIT_FAILURE);
+    // }
+    
+    // ingredientids[0] = 1;
+    // ingredientids[1] = 4;
+    // ingredientids[2] = 7;
+    // changeMeal(1, "Spaghetti bolognese", 50.95, ingredientids);
+
+    
+    // free(ingredientids);
+
+    // ingredients = (Ingredients *) malloc(ingredientssize * sizeof(Ingredients));
+    // if (ingredients == NULL){
+    //     exit(EXIT_FAILURE);
+    // }
+
+    FILE *file = fopen("meals.txt", "r");
+
+    if(file == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    InitializeMeals(file);
+    fclose(file);
+
+}
+
+void InitializeMeals(FILE *file) {
+    // Get number of meals in meals.txt
+    int mealsize = 1;
+    int ch = 0;
+    // Tests the end-of-file indiacator
+    while(!feof(file)) {
+        ch = fgetc(file);
+        // If the last char is breakline do mealsize++
+        if(ch == '\n') {
+            mealsize++;
+        }
+    }
+    printf("%d", mealsize);
+
+    meals = (Meals *) malloc(mealsize * sizeof(Meals));
     if (meals == NULL){
         exit(EXIT_FAILURE);
     }
-    
-    int *ingredientids = (int *) malloc(3 * sizeof(int));
-    if (ingredientids == NULL){
-        exit(EXIT_FAILURE);
+
+    int success;
+    Meals meal;
+    for (int i = 0; i < mealsize; i++) {
+        success = fscanf(file, "%d", &meal.id);
+        printf("success: %d", success);
+        //  if(success != 1) {
+
+        //  }
+        // meal.id = i + 1;
+        meals[i] = meal;
     }
-    
-    ingredientids[0] = 1;
-    ingredientids[1] = 4;
-    ingredientids[2] = 7;
-    changeMeal(1, "Spaghetti bolognese", 50.95, ingredientids);
-    // {2, "Lasagne", 50.95, {1,4,7}},
-    // {3, "Pasta pesto", 50.95, {2,7}},
-    // {4, "Hot dogs", 50.95, {}},
-    // {5, "Pizza", 50.95, {4,6}},
-    // {6, "Chili con carne", 50.95, {3}},
-    // {7, "Indian dahl", 50.95, {5}}
-    
-    free(ingredientids);
 
-    ingredients = (Ingredients *) malloc(ingredientssize * sizeof(Ingredients));
-    if (ingredients == NULL){
-        exit(EXIT_FAILURE);
+    for (int i = 0; i < mealsize; i++) {
+        printf("\nMeal ID: %d\n", meals[i].id);
+        // printf("\nMeal Name: %s\n", meals[i].name);
+        // printf("\nMeal Price: %lf\n", meals[i].price);
     }
-    // Ingredients ingredients[ingredientssize] = {
-    //   {1, "Minced meat", 39.95},
-    //   {2, "Pesto", 15.95},
-    //   {3, "Beans", 22.95},
-    //   {4, "Dolmio", 10.95},
-    //   {5, "Green Lentils", 5.95},
-    //   {6, "Ham", 11.55},
-    //   {7, "Pasta", 9.55},
-    // };
 }
 
-void changeMeal(int id, char* name, double price, int* ingredientids){
-    meals[id - 1].id = id;
-    strcpy(meals[id - 1].name, name);
-    meals[id - 1].price = price;
-    // meals[id - 1].ings = ingredientids;
-}
+// void changeMeal(int id, char* name, double price, int* ingredientids){
+//     meals[id - 1].id = id;
+//     strcpy(meals[id - 1].name, name);
+//     meals[id - 1].price = price;
+//     // meals[id - 1].ings = ingredientids;
+// }
 
-void changeIngredient(int id, char* name, double price){
-    ingredients[id - 1].id = id;
-    strcpy(ingredients[id - 1].name, name);
-    ingredients[id - 1].price = price;
-}
+// void changeIngredient(int id, char* name, double price){
+//     ingredients[id - 1].id = id;
+//     strcpy(ingredients[id - 1].name, name);
+//     ingredients[id - 1].price = price;
+// }
--- mainfile.c
+++ mainfile.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "data.h"
-#include "searchMeals.h"
+
 
 int main(void){
-  printf("Pleeeease skriv 7: %d \n", mealssize);
+  // printf("Pleeeease skriv 7: %d \n", mealssize);
   initializeStructs();
-  printf("main: meal id %d \n", meals[0].id);
-  printf("Tager noget fra første ret: %s", meals[0].name);
+  // printf("main: meal id %d \n", meals[0].id);
+  // printf("Tager noget fra første ret: %s", meals[0].name);
 
-  free(meals);
-  free(ingredients);
-  free(foundmeals);
+  // free(meals);
+  // free(ingredients);
+  // free(foundmeals);
 
   return 0;
 }
