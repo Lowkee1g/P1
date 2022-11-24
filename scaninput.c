66b72bfaf6483cd1937fb5e1d7557124f2123103
Lowkee1g <Loke@doj.dk>
2022-11-24 11:19:24 +0100

    .

--- scaninput.c
+++ scaninput.c
@@ -6,76 +6,38 @@
 char inputString[50];
 char *inputArray;
 
-void BlueOuputColor() {
-    printf("\033[0;36m");
-}
-void ResetOutputColor() {
-    printf("\033[0m");
-}
-void Debug(char s[100]) {
-    printf("\033[0;35m");
-    printf("     %c <%s>",175,s);
-    ResetOutputColor();
-}
-
-int main(void)
-{
-    scanData();
-
-    //Debug("This is an Debug");
-
-    
-    return 0;
-}
-
-
-
 
 void scanData() {
     //Peudokode
     /*Scanne efter et input
     Tager inputtet og ændre længende af arrayet ved at sætte det ind efter
     Loop vi op til start */
+
     int c;
     char **array;
 
-    array = malloc(10 * sizeof *array);
-    for (int i = 0; i <10; i++) {
+    //Opretter 2D array af strings
+    array = malloc(1 * sizeof *array);
+    for (int i = 0; i < 50; i++) {
         array[i] = malloc(sizeof *array[i]);
     }
 
-    
-
-    for (int i = 0; i < 1; i++) {
-        for (int j = 0; j < 1; j++)
-        {
-            array[i][j] = ' ';
-        }
-    }
-
+    //Går gennem inputs fra brugen indtil at der bliver skrevet next
     int size = 0;  
     while (1) {
-        printf("\nInsert a string here: ");
+        printf("\nInsert a string here : ");
         gets(array[size]);
-        Debug(array[size]);
-        if (strcmp(array[size],"next\0") == 0) { break; }
+        if (strcmp(array[size],"next\0") == 0) {array[size] = ' '; break;  }
+
+        //Hvis der ikke bliver indtastet next, udvidre den arrayet med 1
+        array = (char *) realloc(array, 1);
         size++;
-    }
-
-    // numbers = malloc((rounds+1) * sizeof *numbers);
-    // for(int i = 0; i < rounds+1; i++) {
-    //     numbers[i] = malloc(n * sizeof *numbers[i]);
-    // }
+    }    
 
 
-    
+    //Debug
     for (int i = 0; i < size; i++) {
-        printf("\n");
-        for (int j = 0; j <= sizeof(*array)/sizeof(*array[0]); j++)
-        {
-            printf("%c", array[i][j]);
-        }
-        
+        puts(array[i]);
     }
 
 }
