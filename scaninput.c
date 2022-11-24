f707c23963136ade9e2dd18d3a2cb6457f7828cc
Lowkee1g <59569937+Lowkee1g@users.noreply.github.com>
2022-11-24 10:28:00 +0100

    matix

--- scaninput.c
+++ scaninput.c
@@ -22,7 +22,7 @@ int main(void)
 {
     scanData();
 
-    Debug("This is an Debug");
+    //Debug("This is an Debug");
 
     
     return 0;
@@ -36,13 +36,46 @@ void scanData() {
     /*Scanne efter et input
     Tager inputtet og ændre længende af arrayet ved at sætte det ind efter
     Loop vi op til start */
-
-    int **array;
+    int c;
+    char **array;
 
     array = malloc(10 * sizeof *array);
+    for (int i = 0; i <10; i++) {
+        array[i] = malloc(sizeof *array[i]);
+    }
 
-    for (int i = 0; i < )
+    
+
+    for (int i = 0; i < 1; i++) {
+        for (int j = 0; j < 1; j++)
+        {
+            array[i][j] = ' ';
+        }
+    }
+
+    int size = 0;  
+    while (1) {
+        printf("\nInsert a string here: ");
+        gets(array[size]);
+        Debug(array[size]);
+        if (strcmp(array[size],"next\0") == 0) { break; }
+        size++;
+    }
+
+    // numbers = malloc((rounds+1) * sizeof *numbers);
+    // for(int i = 0; i < rounds+1; i++) {
+    //     numbers[i] = malloc(n * sizeof *numbers[i]);
+    // }
 
 
+    
+    for (int i = 0; i < size; i++) {
+        printf("\n");
+        for (int j = 0; j <= sizeof(*array)/sizeof(*array[0]); j++)
+        {
+            printf("%c", array[i][j]);
+        }
+        
+    }
 
 }
