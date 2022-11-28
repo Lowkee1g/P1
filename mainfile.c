d48cf567605ab098b307cadfa44bb516f94d893e
Lowkee1g <Loke@doj.dk>
2022-11-28 15:05:50 +0100

    Kommentar

--- mainfile.c
+++ mainfile.c
@@ -2,5 +2,17 @@
 #include "scaninput.h"
 
 int main(void){
+
+    //Load meals and ingrediences here
+
+    //Scan for data here
+    scanData();
+
+    //Search for meals based of the scan
+
+    //Sort the output here
+
+    //Output the data here
+
     return 0;
 }
