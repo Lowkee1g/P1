410c86054b2250e16d87c5ba072c96264ed31205
Andersraae <andersraae01@gmail.com>
2022-12-06 11:49:27 +0100

    Debugging: searchMeals finder og sammenligner ingredienser, men samler ikke ting sammen

--- scaninput.c
+++ scaninput.c
@@ -5,6 +5,7 @@
 //https://stackoverflow.com/questions/14768230/malloc-for-struct-and-pointer-in-c
 
 char **array;
+int inputSize;
 
 void scanData() {
     //Peudokode
@@ -24,29 +25,28 @@ void scanData() {
 
     //Går gennem inputs fra brugen indtil at der bliver skrevet next
     printf("\nInsert ingredients (Type next to end): ");
-    int size = 0;
+    inputSize = 0;
     while (1) {
-        array[size] = malloc(50 * sizeof(char));
-        if (array[size] == NULL){
+        array[inputSize] = malloc(50 * sizeof(char));
+        if (array[inputSize] == NULL){
             exit(EXIT_FAILURE);
         }
         // scanf("%[^\n]", array[size]);
-        fgets(array[size], 50, stdin); // fgets tager en bestemt størrelse 
-        if (strcmp(array[size],"next\n") == 0) {
+        fgets(array[inputSize], 50, stdin); // fgets tager en bestemt størrelse 
+        if (strcmp(array[inputSize],"next\n") == 0) {
             printf("Break \n");
-            array[size] = (char *) ' '; 
+            array[inputSize] = (char *) ' '; 
             break;
         }
         printf("\nnext ingredient: ");
 
         //Hvis der ikke bliver indtastet next, udvidre den arrayet med 1
-        size++;
+        inputSize++;
     }    
 
 
     //Debug
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < inputSize; i++) {
         puts(array[i]);
     }
-
 }
--- searchMeals.c
+++ searchMeals.c
@@ -10,30 +10,31 @@ void searchMeals();
 int contains(int a, int *list, int size);
 
 void searchMeals(){
-    // int size = sizeof(array)/sizeof(array);
-    Meals **foundmeals = (Meals **) malloc(50 * sizeof(Meals)); 
+    Meals **foundmeals = (Meals **) malloc(inputSize * sizeof(Meals)); 
     if (foundmeals == NULL){
-        printf("searchMeals exit failture linje 15 \n");
+        printf("searchMeals exit failure linje 15 \n");
         exit(EXIT_FAILURE);
     }
-    printf("foundmeals oprettet \n");
 
-    int *ingids = (int *) malloc(50 * sizeof(int));
+    int *ingids = (int *) malloc(inputSize * sizeof(int));
     if (ingids == NULL){
-        printf("searchMeals exit failture linje 21 \n");
+        printf("searchMeals exit failure linje 21 \n");
         exit(EXIT_FAILURE);
     }
-    printf("ingsid oprettet \n");
     
-    for (int i = 0; i < 50; i++){ // For each input ingredient
+    for (int i = 0; i < inputSize; i++){ // For each input ingredient
+    printf("Første løkke og her er ingssize %d og inputSize er %d \n", ingredientsSize, inputSize);
         for (int j = 0; j < ingredientsSize; j++){ // For each existing ingredient struct
+        printf("2");
             if(!strcmp(array[i], ingredients[j].name)){ // Find the given ingredients id.
+            printf("Der blev fundet %s og %s som matcher", array[i], ingredients[j].name);
                 ingids[i] = ingredients[j].id;
             }             
         }
+        printf("\n");
     }
 
-    for (int i = 0; i < 50; i++){ // Make a list of meals for each ingredient
+    for (int i = 0; i < inputSize; i++){ // Make a list of meals for each ingredient
         foundmeals[i] = (Meals *) malloc(mealSize * sizeof(Meals));
         if (foundmeals == NULL){
             printf("searchMeals exit failture linje 36 \n");
@@ -42,7 +43,7 @@ void searchMeals(){
     }
 
     int counter; 
-    for (int i = 0; i < 50 ; i++){ // Iterate through list of ingredients
+    for (int i = 0; i < inputSize ; i++){ // Iterate through list of ingredients
     counter = 0;
         for (int j = 0; j < mealSize; j++) { // Iterate through all meals to find the meals with matching ingredients
             if (contains(ingids[i], meals[j].ings, meals[j].sizeOfIngs)){
@@ -55,12 +56,9 @@ void searchMeals(){
 }
 
 int contains(int a, int *list, int size){
-    printf("I contains \n");
-    int isElementPresent = 0;
-
     for (int i = 0; i < size; i++) {
         if (list[i] == a) {
-            isElementPresent = 1;
+            printf("\n \n \ncontains returnerer 1 \n \n \n");
             return 1;
         }
     }
