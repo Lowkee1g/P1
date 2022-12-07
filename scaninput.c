520a805f5f7bc22060333d4a7b0ac7a5995ea02f
Lowkee1g <59569937+Lowkee1g@users.noreply.github.com>
2022-12-07 09:01:09 +0100

    malloc fix

--- scaninput.c
+++ scaninput.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "scaninput.h"
+//#include "scaninput.h"
 //https://stackoverflow.com/questions/14768230/malloc-for-struct-and-pointer-in-c
 
 char **array;
+int size = 0;
+
+int main(void)
+{
+    scanData();
+
+
+    for (int i = 0; i < size; i++)
+    {
+        free(array[i]); // free memory for each string in the array
+    }
+    free(array);
+
+    return 0;
+
+    
+}
+
 
 void scanData() {
     //Peudokode
@@ -15,7 +33,7 @@ void scanData() {
     //int c;
 
     //Opretter 2D array af strings
-    array = malloc(50 * sizeof(char)); 
+    array = malloc(1 * sizeof(char *)); 
     if (array == NULL)
     {
         exit(EXIT_FAILURE);
@@ -24,29 +42,37 @@ void scanData() {
 
     //Går gennem inputs fra brugen indtil at der bliver skrevet next
     printf("\nInsert ingredients (Type next to end): ");
-    int size = 0;
+
     while (1) {
+        printf("size - %d\n", size);
         array[size] = malloc(50 * sizeof(char));
+        printf(" after");
         if (array[size] == NULL){
+            printf("Exit here - %d", size);
             exit(EXIT_FAILURE);
         }
         // scanf("%[^\n]", array[size]);
         fgets(array[size], 50, stdin); // fgets tager en bestemt størrelse 
+        
         if (strcmp(array[size],"next\n") == 0) {
             printf("Break \n");
             array[size] = (char *) ' '; 
             break;
         }
+
         printf("\nnext ingredient: ");
 
         //Hvis der ikke bliver indtastet next, udvidre den arrayet med 1
         size++;
-    }    
+        array = realloc(array, (1 + size) * sizeof(char *));
+    }
 
 
     //Debug
+    printf("size of Malloc: %d\n", size);
+
     for (int i = 0; i < size; i++) {
-        puts(array[i]);
+        printf("%d - %s",i,array[i]);
     }
 
 }
